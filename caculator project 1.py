adff97aea08bb99a54b15dd88190706bd8045654
Alirue <36883498+Alirue@users.noreply.github.com>
2018-03-23 13:32:19 +0200

    Add files via upload

---
+++ caculator project 1.py
@@ -0,0 +1,30 @@
+print("welecome to calculator")
+num1=int(input("enter your first no: "))
+fun=input("function +, -, /, *, = : ")
+num2=int(input("enter your second no: "))    
+    
+if fun=="+":
+    ans=num1+num2
+    print(ans)
+else:
+      True
+
+if fun=="-":
+    ans=num1-num2
+    print(ans)
+else:
+      True
+
+if fun=="/":
+    ans=num1/num2
+    print(ans)
+else:
+      True      
+
+if fun=="*":
+    ans=num1*num2
+    print(ans)
+else:
+      True
+
+
